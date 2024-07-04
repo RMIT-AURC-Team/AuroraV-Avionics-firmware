fada91bb9d19d633802ecace280892df4a00cd52
Matt <mattricci6@gmail.com>
2024-07-05 03:51:29 +1000

    Implement RTOS with integration for hardware, calls to initialise and sample sensor suite

--- AuroraV-Avionics/Core/Inc/main.h
+++ AuroraV-Avionics/Core/Inc/main.h
@@ -24,23 +24,16 @@
 
 #include "FreeRTOS.h"
 #include "event_groups.h"
-#include "init.h"
-#include "main.h"
 #include "stdint.h"
 #include "stm32f4xx.h"
 #include "task.h"
+
+#include "drivers.h"
 
 #include "kalmanfilter.h"
 #include "membuff.h"
 #include "quaternion.h"
 #include "state.h"
-#include "uart.h"
-
-#include "baccel.h"
-#include "bbaro.h"
-#include "gyroX.h"
-#include "gyroY.h"
-#include "gyroZ.h"
 
 void vFlashBuffer(void *pvParameters);
 void vDataAcquisitionH(void *pvParameters);
@@ -48,6 +41,32 @@ void vDataAcquisitionL(void *pvParameters);
 void vStateUpdate(void *pvParameters);
 
 void Error_Handler(void);
+
+#define GYRO_SENSITIVITY           (0.00875f)
+#define GYRO_CTRL_REG1             0x20
+#define GYRO_CTRL_REG1_ODR_800Hz   0xC0
+#define GYRO_CTRL_REG1_PD_ENABLE   0x08
+#define GYRO_CTRL_REG1_AXIS_ENABLE 0x07
+
+#define MAGNET_SENSITIVITY 			(1.0f / 1711.0f)
+#define MAGNET_CTRL_REG1 	 			0x20
+#define MAGNET_CTRL_REG1_FAST 	0x02
+#define MAGNET_CTRL_REG2 				0x21
+#define MAGNET_CTRL_REG2_FS16   0x60
+
+#define ACCEL_SENSITIVITY_32G (1.0f / 1024.0f)
+#define ACCEL_CNTL1           0x1B
+#define ACCEL_ODCNTL          0x21
+#define ACCEL_ODCNTL_RESERVED 0x90
+
+#define BARO_TEMP_SENSITIVITY  (1.0f / 65535)
+#define BARO_PRESS_SENSITIVITY (1.0f / 64)
+#define BARO_ODR_CFG 					 0x37
+#define BARO_ODR_CFG_PWR 			 0x01
+#define BARO_ODR_CFG_DEEP_DIS	 0x80
+#define BARO_OSR_CFG_RESERVED  0x80
+#define BARO_OSR_CFG 					 0x36
+#define BARO_OSR_CFG_PRESS_EN  0x40
 
 // Dataframe header definitions
 #define HEADER_ID_Pos     0x06
--- AuroraV-Avionics/Core/Src/main.c
+++ AuroraV-Avionics/Core/Src/main.c
@@ -14,12 +14,17 @@ float zUnit[3]     = {0, 0, 1}; // Z unit vector
 float cosine       = 0;         // Tilt angle cosine
 float tilt         = 0;         // Tilt angle
 
-// Flight dynamics state variables
-float altitude = 0;                  // Current altitude
-float velocity = 0;                  // Current vertical velocity
-float accel    = 0;                  // Current body-axis acceleration
+// Angular rotations
+float roll  = 0;
+float pitch = 0;
+float yaw   = 0;
 
-#define BUFF_SIZE 21000              // 2s worth of data in buffer
+// Flight dynamics state variables
+float altitude = 0;     // Current altitude
+float velocity = 0;     // Current vertical velocity
+float accel    = 0;     // Current body-axis acceleration
+
+#define BUFF_SIZE 21000 // 2s worth of data in buffer
 #define PAGE_SIZE 256
 MemBuff mem;
 uint8_t buff[BUFF_SIZE];
@@ -27,17 +32,49 @@ uint8_t outBuff[PAGE_SIZE];
 uint8_t buffCount = 0;               // Test variable for reading output buffer
 
 enum State currentState = PRELAUNCH; // Boot in prelaunch
-void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName);
-void initHeartbeat();
-int interval2ms = 0;
+int interval2ms         = 0;
+
+const struct LoRa_Registers LoRa_Regs = {0, 1, 0xd, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0X1C, 0X1D, 0X1E, 0X1F, 0X20, 0X21, 0X22, 0X23, 0X24, 0X25, 0X28, 0X29, 0X2A, 0X2C, 0X31, 0X33, 0X37, 0X39, 0X3B, 0X3D, 0X40, 0X41};
 
 int main(void) {
-  //  configure_RCC_APB1();
-  //  configure_RCC_APB2();
-  //  configure_RCC_AHB1();
   SystemInit();
-  initUART();
-  initHeartbeat();
+
+  // Bring up RCC
+  configure_RCC_APB1();
+  configure_RCC_APB2();
+  configure_RCC_AHB1();
+  configure_MISC_GPIO();
+  configure_UART3_GPS();
+  configure_UART6_Serial_2_mini_USB();
+  configure_SPI1_Sensor_Suite();
+  configure_SPI2_Flash();
+  configure_SPI3_LoRa();
+
+  // Initialise timers
+  TIM6init();
+  TIM7init();
+
+  // Configure peripherals
+  CANGPIO_config();
+  configure_LoRa_module(&LoRa_Regs);
+
+  // Configure accelerometer
+  write_ACCELL_1(ACCEL_CNTL1, 0x50);                                     // Accel select, 32g sensitivity
+  uint8_t ODCNTL = read_ACCELL_1(ACCEL_ODCNTL);                          // Read from register for reserve mask
+  write_ACCELL_1(ACCEL_ODCNTL, (ACCEL_ODCNTL_RESERVED & ODCNTL) | 0x2A); // No filter, fast startup, 800Hz
+  write_ACCELL_1(ACCEL_CNTL1, 0xD0);                                     // Enable PC1
+
+  // Configure gyroscope
+  write_GYRO(GYRO_CTRL_REG1, GYRO_CTRL_REG1_ODR_800Hz | GYRO_CTRL_REG1_AXIS_ENABLE | GYRO_CTRL_REG1_PD_ENABLE);
+
+  // Configure magnetometer
+  write_MAG(MAGNET_CTRL_REG1, MAGNET_CTRL_REG1_FAST);
+  write_MAG(MAGNET_CTRL_REG2, MAGNET_CTRL_REG2_FS16);
+
+  // Configure barometer
+  write_BARO(BARO_ODR_CFG, BARO_ODR_CFG_PWR | BARO_ODR_CFG_DEEP_DIS);
+  uint8_t OSRCFG = read_BARO(BARO_OSR_CFG);
+  write_BARO(BARO_OSR_CFG, (BARO_OSR_CFG_RESERVED * OSRCFG) | BARO_OSR_CFG_PRESS_EN);
 
   MemBuff_init(&mem, buff, BUFF_SIZE, PAGE_SIZE);
   Quaternion_init(&qRot);
@@ -70,7 +107,7 @@ void vApplicationIdleHook(void) {
 }
 
 void vFlashBuffer(void *argument) {
-  const TickType_t timeout = 10;
+  const TickType_t timeout = pdMS_TO_TICKS(1);
   for (;;) {
     // Wait for write flag to be ready, clear flag on exit
     EventBits_t uxBits = xEventGroupWaitBits(xTaskEnableGroup, 0x01, pdTRUE, pdFALSE, timeout);
@@ -85,17 +122,17 @@ void vFlashBuffer(void *argument) {
  * ===================================================================== */
 
 void vStateUpdate(void *argument) {
-  int periods = 0;
+  // int periods = 0;
   TickType_t xLastWakeTime;
   const TickType_t xFrequency = pdMS_TO_TICKS(20); // 50Hz
+
   for (;;) {
+    // Block until 20ms interval
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    if (interval2ms % 500 == 0)
-      GPIOB->ODR ^= GPIO_ODR_OD0;                  // Green
 
     // Increment flight timer period post-launch
     // if (currentState != PRELAUNCH)
-    periods++;
+    //   periods++;
 
     switch (currentState) {
     case PRELAUNCH:
@@ -144,46 +181,65 @@ void vStateUpdate(void *argument) {
  * ===================================================================== */
 
 void vDataAcquisitionH(void *argument) {
-  unsigned int index = 0;
-  float dt           = 0.002;
+  float dt = 0.002;
   float roll, pitch, yaw;
 
   TickType_t xLastWakeTime;
   const TickType_t xFrequency = pdMS_TO_TICKS(2); // 500Hz
   const TickType_t blockTime  = pdMS_TO_TICKS(0); // Don't need to block
   for (;;) {
+    // Block until 2ms interval
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    // Heartbeat
-    if (interval2ms % 50 == 0)
-      GPIOB->ODR ^= GPIO_ODR_OD14; // Red
-    interval2ms++;
 
-    // Read sensor data
-    memcpy(&accel, baccel + index, sizeof(accel));
-    memcpy(&roll, gyroX + index, sizeof(roll));
-    memcpy(&pitch, gyroY + index, sizeof(pitch));
-    memcpy(&yaw, gyroZ + index, sizeof(yaw));
+    // Read and process accelerometer
+    uint8_t lAccelX = read_ACCELL_1(0x08); // Accel X low
+    uint8_t hAccelX = read_ACCELL_1(0x09); // Accel X high
+    uint8_t lAccelY = read_ACCELL_1(0x0A); // Accel Y low
+    uint8_t hAccelY = read_ACCELL_1(0x0B); // Accel Y high
+    uint8_t lAccelZ = read_ACCELL_1(0x0C); // Accel Z low
+    uint8_t hAccelZ = read_ACCELL_1(0x0D); // Accel Z high
+    // Need to adjust this to actual axis
+    accel = ACCEL_SENSITIVITY_32G * (int16_t)(((uint16_t)hAccelZ << 8) | lAccelZ);
+
+    // Read and process gyroscope
+    uint8_t lGyroX = read_GYRO(0x28); // Gyro X low
+    uint8_t hGyroX = read_GYRO(0x29); // Gyro X high
+    uint8_t lGyroY = read_GYRO(0x2A); // Gyro Y low
+    uint8_t hGyroY = read_GYRO(0x2B); // Gyro Y high
+    uint8_t lGyroZ = read_GYRO(0x2C); // Gyro Z low
+    uint8_t hGyroZ = read_GYRO(0x2D); // Gyro Z high
+    // Need to adjust this to actual axis
+    roll  = GYRO_SENSITIVITY * (int16_t)(((uint16_t)hGyroX << 8) | lGyroX);
+    pitch = GYRO_SENSITIVITY * (int16_t)(((uint16_t)hGyroY << 8) | lGyroY);
+    yaw   = GYRO_SENSITIVITY * (int16_t)(((uint16_t)hGyroZ << 8) | lGyroZ);
+
+    uint8_t lMagnetX = read_MAG(0x28); // Mag X low
+    uint8_t hMagnetX = read_MAG(0x29); // Mag X high
+    uint8_t lMagnetY = read_MAG(0x2A); // Mag Y low
+    uint8_t hMagnetY = read_MAG(0x2B); // Mag Y high
+    uint8_t lMagnetZ = read_MAG(0x2C); // Mag Z low
+    uint8_t hMagnetZ = read_MAG(0x2D); // Mag Z high
 
     // Add sensor data and quaternion to dataframe
     mem.append(&mem, HEADER_HIGHRES);
-    mem.append(&mem, buffCount++); // Accel X high byte
-    mem.append(&mem, buffCount++); // Accel X low byte
-    mem.append(&mem, buffCount++); // Accel Y high byte
-    mem.append(&mem, buffCount++); // Accel Y low byte
-    mem.append(&mem, buffCount++); // Accel Z high byte
-    mem.append(&mem, buffCount++); // Accel Z low byte
-    mem.append(&mem, buffCount++); // Gyro X high byte
-    mem.append(&mem, buffCount++); // Gyro X low byte
-    mem.append(&mem, buffCount++); // Gyro Y high byte
-    mem.append(&mem, buffCount++); // Gyro Y low byte
-    mem.append(&mem, buffCount++); // Gyro Z high byte
-    mem.append(&mem, buffCount++); // Gyro Z low byte
-    mem.append(&mem, buffCount++); // Magnet X high byte
-    mem.append(&mem, buffCount++); // Magnet X low byte
-    mem.append(&mem, buffCount++); // Magnet Y high byte
-    mem.append(&mem, buffCount++); // Magnet Y low byte
-    mem.append(&mem, buffCount++); // Magnet Z high byte
-    mem.append(&mem, buffCount++); // Magnet Z low byte
+    mem.append(&mem, hAccelX);  // Accel X high byte
+    mem.append(&mem, lAccelX);  // Accel X low byte
+    mem.append(&mem, hAccelY);  // Accel Y high byte
+    mem.append(&mem, lAccelY);  // Accel Y low byte
+    mem.append(&mem, hAccelZ);  // Accel Z high byte
+    mem.append(&mem, lAccelZ);  // Accel Z low byte
+    mem.append(&mem, hGyroX);   // Gyro X high byte
+    mem.append(&mem, lGyroX);   // Gyro X low byte
+    mem.append(&mem, hGyroY);   // Gyro Y high byte
+    mem.append(&mem, lGyroY);   // Gyro Y low byte
+    mem.append(&mem, hGyroZ);   // Gyro Z high byte
+    mem.append(&mem, lGyroZ);   // Gyro Z low byte
+    mem.append(&mem, hMagnetX); // Magnet X high byte
+    mem.append(&mem, lMagnetX); // Magnet X low byte
+    mem.append(&mem, hMagnetY); // Magnet Y high byte
+    mem.append(&mem, lMagnetY); // Magnet Y low byte
+    mem.append(&mem, hMagnetZ); // Magnet Z high byte
+    mem.append(&mem, lMagnetZ); // Magnet Z low byte
 
     // Only run calculations when enabled
     EventBits_t uxBits = xEventGroupWaitBits(xTaskEnableGroup, 0x02, pdFALSE, pdFALSE, blockTime);
@@ -208,8 +264,6 @@ void vDataAcquisitionH(void *argument) {
       cosine = zUnit[0] * vAttitude[0] + zUnit[1] * vAttitude[1] + zUnit[2] * vAttitude[2];
       tilt   = acos(cosine) * 180 / M_PI;
     }
-
-    index = (index + 4) % accel_length; // Handle buffer overflow;
   }
 }
 
@@ -218,9 +272,7 @@ void vDataAcquisitionH(void *argument) {
  * ===================================================================== */
 
 void vDataAcquisitionL(void *argument) {
-  unsigned int index = 0;
-  float dt           = 0.020;
-
+  float dt = 0.020;
   KalmanFilter kf;
   KalmanFilter_init(&kf);
 
@@ -257,14 +309,21 @@ void vDataAcquisitionL(void *argument) {
   TickType_t xLastWakeTime;
   const TickType_t xFrequency = pdMS_TO_TICKS(20); // 50Hz
   const TickType_t blockTime  = pdMS_TO_TICKS(0);  // Don't need to block
+
   for (;;) {
+    // Block until 20ms interval
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    // Heartbeat
-    if (interval2ms % 500 == 0)
-      GPIOB->ODR ^= GPIO_ODR_OD7; // Blue
-    //
-    // Read sensor data
-    memcpy(&altitude, baro + index, sizeof(altitude));
+
+    // Read baro data
+    uint8_t hTemp = read_BARO(0x1F);
+    uint8_t lTemp = read_BARO(0x1E);
+    uint8_t xTemp = read_BARO(0x1D);
+    float temp    = BARO_TEMP_SENSITIVITY * (((int32_t)hTemp << 16) | ((int32_t)lTemp << 8) | xTemp);
+
+    uint8_t hPress = read_BARO(0x22);
+    uint8_t lPress = read_BARO(0x21);
+    uint8_t xPress = read_BARO(0x20);
+    float press    = BARO_PRESS_SENSITIVITY * (((int32_t)hPress << 16) | ((int32_t)lPress << 8) | xPress);
 
     // Add sensor data and quaternion to dataframe
     mem.append(&mem, HEADER_LOWRES);
@@ -291,41 +350,7 @@ void vDataAcquisitionL(void *argument) {
       } u;
       u.f = kf.x.pData[1];
     }
-
-    index = (index + 4) % baro_length; // Handle buffer overflow;
   }
-}
-
-void initHeartbeat() {
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-  __ASM("NOP");
-  __ASM("NOP");
-  GPIOB->MODER   &= (~(GPIO_MODER_MODE0_Msk));
-  GPIOB->MODER   |= ((0x1 << GPIO_MODER_MODE0_Pos));
-  GPIOB->OTYPER  &= (uint16_t)(~(GPIO_OTYPER_OT0));
-  GPIOB->OSPEEDR &= (~(GPIO_OSPEEDR_OSPEED0_Msk));
-  GPIOB->OSPEEDR |= ((0x2 << GPIO_OSPEEDR_OSPEED0_Pos));
-  GPIOB->ODR     &= ~GPIO_ODR_OD0;
-
-  GPIOB->MODER   &= (~(GPIO_MODER_MODE7_Msk));
-  GPIOB->MODER   |= ((0x1 << GPIO_MODER_MODE7_Pos));
-  GPIOB->OTYPER  &= (uint16_t)(~(GPIO_OTYPER_OT7));
-  GPIOB->OSPEEDR &= (~(GPIO_OSPEEDR_OSPEED7_Msk));
-  GPIOB->OSPEEDR |= ((0x2 << GPIO_OSPEEDR_OSPEED7_Pos));
-  GPIOB->ODR     &= ~GPIO_ODR_OD7;
-
-  GPIOB->MODER   &= (~(GPIO_MODER_MODE14_Msk));
-  GPIOB->MODER   |= ((0x1 << GPIO_MODER_MODE14_Pos));
-  GPIOB->OTYPER  &= (uint16_t)(~(GPIO_OTYPER_OT14));
-  GPIOB->OSPEEDR &= (~(GPIO_OSPEEDR_OSPEED14_Msk));
-  GPIOB->OSPEEDR |= ((0x2 << GPIO_OSPEEDR_OSPEED14_Pos));
-  GPIOB->ODR     &= ~GPIO_ODR_OD14;
-}
-
-void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName) {
-  xTaskHandle *bad_task_handle = pxTask;
-  signed char *bad_task_name   = pcTaskName;
-  for (;;);
 }
 
 // Unsure of actual fix for linker error
