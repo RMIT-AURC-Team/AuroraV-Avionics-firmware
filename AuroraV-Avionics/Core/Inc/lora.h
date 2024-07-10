b768429e1ed8f267a11358d9f519b4c58517a73b
Matt <mattricci6@gmail.com>
2024-07-10 18:33:46 +1000

    Isolate LoRa drivers to separate source files

--- AuroraV-Avionics/Core/Inc/lora.h
+++ AuroraV-Avionics/Core/Inc/lora.h
@@ -3,71 +3,6 @@
 
 #include "stm32f439xx.h"
 
-void configure_SPI3_LoRa();
-void Load_And_Send_To_LoRa(char *pointerdata);
-void configure_LoRa_module();
-void write_lora_packet(uint8_t address, uint8_t payload);
-uint8_t receive_lora_data(uint8_t address);
-
-// =========================================================
-//               SPI DATA PINS (SCLK, SDI, SDO)
-// =========================================================
-#define LORA_DATAC_AFRH_Msk   (GPIO_AFRH_AFRH2 | GPIO_AFRH_AFRH3 | GPIO_AFRH_AFRH4)
-#define LORA_DATAC_OTYPE_Msk  (GPIO_OTYPER_OT10_Msk | GPIO_OTYPER_OT11_Msk | GPIO_OTYPER_OT12_Msk)
-#define LORA_DATAC_MODE_Msk   (GPIO_MODER_MODE10_Msk | GPIO_MODER_MODE11_Msk | GPIO_MODER_MODE12_Msk)
-#define LORA_DATAC_PUPD_Msk   (GPIO_PUPDR_PUPD10_Msk | GPIO_PUPDR_PUPD11_Msk | GPIO_PUPDR_PUPD12_Msk)
-#define LORA_DATAC_OSPEED_Msk (GPIO_OSPEEDR_OSPEED10_Msk | GPIO_OSPEEDR_OSPEED11_Msk | GPIO_OSPEEDR_OSPEED12_Msk)
-
-#define LORA_CONTROLD_MODE_Msk (GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE7_Msk)
-#define LORA_DATAD_MODE_Msk    (GPIO_MODER_MODE1_Msk)
-
-// clang-format off
-#define LORA_DATAC_MODE_Set(value) ( \
-    value << GPIO_MODER_MODE10_Pos   \
-  | value << GPIO_MODER_MODE11_Pos   \
-  | value << GPIO_MODER_MODE12_Pos   \
-)
-
-#define LORA_DATAC_PUPD_Set(value) ( \
-    value << GPIO_PUPDR_PUPD10_Pos   \
-  | value << GPIO_PUPDR_PUPD11_Pos   \
-  | value << GPIO_PUPDR_PUPD12_Pos   \
-)
-
-#define LORA_DATAC_SPEED_Set(value) (  \
-    value << GPIO_OSPEEDR_OSPEED10_Pos \
-  | value << GPIO_OSPEEDR_OSPEED11_Pos \
-  | value << GPIO_OSPEEDR_OSPEED12_Pos \
-)
-
-#define LORA_DATAC_AFRH_Set(value) ( \
-    value << GPIO_AFRH_AFSEL10_Pos   \
-  | value << GPIO_AFRH_AFSEL11_Pos   \
-  | value << GPIO_AFRH_AFSEL12_Pos   \
-)
-// clang-format on
-
-// =========================================================
-//              SPI CONTROL PINS (WP, HOLD, CS)
-// =========================================================
-#define LORA_CONTROL_ODR_Msk    (GPIO_ODR_OD9_Msk | GPIO_ODR_OD10_Msk | GPIO_ODR_OD11_Msk)
-#define LORA_CONTROL_OTYPE_Msk  (GPIO_OTYPER_OT9_Msk | GPIO_OTYPER_OT10_Msk | GPIO_OTYPER_OT11_Msk)
-#define LORA_CONTROL_MODE_Msk   (GPIO_MODER_MODE9_Msk | GPIO_MODER_MODE10_Msk | GPIO_MODER_MODE11_Msk)
-#define LORA_CONTROL_OSPEED_Msk (GPIO_OSPEEDR_OSPEED9_Msk | GPIO_OSPEEDR_OSPEED10_Msk | GPIO_OSPEEDR_OSPEED11_Msk)
-
-// clang-format off
-#define LORA_CONTROLD_MODE_Set(value) ( \
-    value << GPIO_MODER_MODE0_Pos       \
-  | value << GPIO_MODER_MODE7_Pos       \
-)
-
-#define LORA_CONTROLC_TYPE_Set(value) ( \
-    value << GPIO_OTYPE_OT9_Pos   \
-  | value << GPIO_OTYPE_OT10_Pos  \
-  | value << GPIO_OTYPE_OT11_Pos  \
-)
-// clang-format on
-
 struct LoRa_Registers {
   uint8_t RegFifo;
   uint8_t RegOpMode;
@@ -109,6 +44,70 @@ struct LoRa_Registers {
   uint8_t RegDioMapping1;
   uint8_t RegDioMapping2;
 };
-extern const struct LoRa_Registers LoRa_Registers;
+
+void configure_SPI3_LoRa();
+void Load_And_Send_To_LoRa(char *pointerdata, const struct LoRa_Registers *LoRa_Registers);
+void configure_LoRa_module();
+void write_lora_packet(uint8_t address, uint8_t payload);
+uint8_t receive_lora_data(uint8_t address);
+
+// =========================================================
+//               SPI DATA PINS (SCLK, SDI, SDO)
+// =========================================================
+#define LORA_DATAC_AFRH_Msk   (GPIO_AFRH_AFRH2 | GPIO_AFRH_AFRH3 | GPIO_AFRH_AFRH4)
+#define LORA_DATAC_OTYPE_Msk  (GPIO_OTYPER_OT10_Msk | GPIO_OTYPER_OT11_Msk | GPIO_OTYPER_OT12_Msk)
+#define LORA_DATAC_MODE_Msk   (GPIO_MODER_MODE10_Msk | GPIO_MODER_MODE11_Msk | GPIO_MODER_MODE12_Msk)
+#define LORA_DATAC_PUPD_Msk   (GPIO_PUPDR_PUPD10_Msk | GPIO_PUPDR_PUPD11_Msk | GPIO_PUPDR_PUPD12_Msk)
+#define LORA_DATAC_OSPEED_Msk (GPIO_OSPEEDR_OSPEED10_Msk | GPIO_OSPEEDR_OSPEED11_Msk | GPIO_OSPEEDR_OSPEED12_Msk)
+
+#define LORA_CONTROLD_MODE_Msk (GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE7_Msk)
+#define LORA_DATAD_MODE_Msk    (GPIO_MODER_MODE1_Msk)
+
+// clang-format off
+#define LORA_DATAC_MODE_Set(value) (   \
+    value << GPIO_MODER_MODE10_Pos     \
+  | value << GPIO_MODER_MODE11_Pos     \
+  | value << GPIO_MODER_MODE12_Pos     \
+)
+
+#define LORA_DATAC_PUPD_Set(value) (   \
+    value << GPIO_PUPDR_PUPD10_Pos     \
+  | value << GPIO_PUPDR_PUPD11_Pos     \
+  | value << GPIO_PUPDR_PUPD12_Pos     \
+)
+
+#define LORA_DATAC_SPEED_Set(value) (  \
+    value << GPIO_OSPEEDR_OSPEED10_Pos \
+  | value << GPIO_OSPEEDR_OSPEED11_Pos \
+  | value << GPIO_OSPEEDR_OSPEED12_Pos \
+)
+
+#define LORA_DATAC_AFRH_Set(value) (   \
+    value << GPIO_AFRH_AFSEL10_Pos     \
+  | value << GPIO_AFRH_AFSEL11_Pos     \
+  | value << GPIO_AFRH_AFSEL12_Pos     \
+)
+// clang-format on
+
+// =========================================================
+//              SPI CONTROL PINS (WP, HOLD, CS)
+// =========================================================
+#define LORA_CONTROL_ODR_Msk    (GPIO_ODR_OD9_Msk | GPIO_ODR_OD10_Msk | GPIO_ODR_OD11_Msk)
+#define LORA_CONTROL_OTYPE_Msk  (GPIO_OTYPER_OT9_Msk | GPIO_OTYPER_OT10_Msk | GPIO_OTYPER_OT11_Msk)
+#define LORA_CONTROL_MODE_Msk   (GPIO_MODER_MODE9_Msk | GPIO_MODER_MODE10_Msk | GPIO_MODER_MODE11_Msk)
+#define LORA_CONTROL_OSPEED_Msk (GPIO_OSPEEDR_OSPEED9_Msk | GPIO_OSPEEDR_OSPEED10_Msk | GPIO_OSPEEDR_OSPEED11_Msk)
+
+// clang-format off
+#define LORA_CONTROLD_MODE_Set(value) ( \
+    value << GPIO_MODER_MODE0_Pos       \
+  | value << GPIO_MODER_MODE7_Pos       \
+)
+
+#define LORA_CONTROLC_TYPE_Set(value) ( \
+    value << GPIO_OTYPE_OT9_Pos         \
+  | value << GPIO_OTYPE_OT10_Pos        \
+  | value << GPIO_OTYPE_OT11_Pos        \
+)
+// clang-format on
 
 #endif
