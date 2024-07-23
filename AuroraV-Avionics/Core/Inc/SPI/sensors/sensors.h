1607b9ba1c2c1edbfc915b00eb7ecc7c7f36b183
Matt <mattricci6@gmail.com>
2024-07-24 01:41:37 +1000

    Minor housekeeping cleanup

--- AuroraV-Avionics/Core/Inc/SPI/sensors/sensors.h
+++ AuroraV-Avionics/Core/Inc/SPI/sensors/sensors.h
@@ -29,35 +29,6 @@ extern const uint8_t ACCEL_AXES_1[3];
 extern const uint8_t ACCEL_AXES_2[3];
 extern const uint8_t GYRO_AXES[3];
 
-// TODO: Get rid of all this
-#define SENSOR_MULTI_SENSITIVITIES_MAXN 3
-
-typedef struct Sensor {
-  unsigned long cs;
-  float sensitivity;
-  void (*write)(struct Sensor *, uint8_t, uint8_t);
-  uint8_t (*read)(struct Sensor *, uint8_t);
-} Sensor;
-
-// Vararg struct type for sensors with multiple outputs with varying sensitivity
-// (damn barometer)
-typedef struct Sensor_multi {
-  unsigned long cs;
-  float sensitivities[SENSOR_MULTI_SENSITIVITIES_MAXN];
-  void (*write)(struct Sensor_multi *, uint8_t, uint8_t);
-  uint8_t (*read)(struct Sensor_multi *, uint8_t);
-} Sensor_multi;
-
-#define MAGNET_SENSITIVITY    (1.0f / 1711.0f)
-#define MAGNET_CTRL_REG1      0x20
-#define MAGNET_CTRL_REG1_FAST 0x02
-#define MAGNET_CTRL_REG2      0x21
-#define MAGNET_CTRL_REG2_FS16 0x60
-
 void configure_SPI1_Sensor_Suite();
-void configure_Sensors();
-
-void write_MAG(uint8_t, uint8_t);
-uint8_t read_MAG(uint8_t);
 
 #endif
