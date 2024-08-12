c9bc919c30f36ffa66f3de5986546ea7a4b5a30c
s3785111 <s3785111@users.noreply.github.com>
2024-08-12 02:52:14 +0000

    deploy: 3b229b2c913dc50ffa5128ebdfecf853f3130d96

--- docs/html/group__LoRa.js
+++ docs/html/group__LoRa.js
@@ -7,5 +7,6 @@ var group__LoRa =
     ] ],
     [ "LoRa_init", "group__LoRa.html#ga1a62293fe572d94b7196ddb6619e2f32", null ],
     [ "LoRa_transmit", "group__LoRa.html#gae19b9f0cd6a296c35d492f391278cecd", null ],
-    [ "LoRa_AVD1", "group__LoRa.html#ga14d114988f8e46c0f9300f4bad30638b", null ]
+    [ "LoRa_AVD1", "group__LoRa.html#ga7e837a74df47b88a7064df74650176ae", null ],
+    [ "LoRa_AVD2", "group__LoRa.html#ga4811652f4eab94cc6063d3eb68bf21e3", null ]
 ];
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['lora_5favd1_0',['LoRa_AVD1',['../group__LoRa.html#ga14d114988f8e46c0f9300f4bad30638b',1,'lora.c']]],
-  ['lora_5finit_1',['LoRa_init',['../group__LoRa.html#ga1a62293fe572d94b7196ddb6619e2f32',1,'lora.c']]],
-  ['lora_5ftransmit_2',['LoRa_transmit',['../group__LoRa.html#gae19b9f0cd6a296c35d492f391278cecd',1,'lora.c']]]
+  ['lora_5favd1_0',['LoRa_AVD1',['../group__LoRa.html#ga7e837a74df47b88a7064df74650176ae',1,'lora.c']]],
+  ['lora_5favd2_1',['LoRa_AVD2',['../group__LoRa.html#ga4811652f4eab94cc6063d3eb68bf21e3',1,'lora.c']]],
+  ['lora_5finit_2',['LoRa_init',['../group__LoRa.html#ga1a62293fe572d94b7196ddb6619e2f32',1,'lora.c']]],
+  ['lora_5ftransmit_3',['LoRa_transmit',['../group__LoRa.html#gae19b9f0cd6a296c35d492f391278cecd',1,'lora.c']]]
 ];
